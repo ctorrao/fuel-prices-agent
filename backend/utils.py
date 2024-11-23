f46eae9a0ba463036b06c278f49817d59b6ce569
Carlos Torrao <carlos.torrao@gmail.com>
2024-11-23 06:09:49 +0000

    Add error handling for API fuel price response to raise exception on non-success status codes

--- backend/utils.py
+++ backend/utils.py
@@ -1,5 +1,8 @@
 def process_api_fuel_price_response(response, max_fuel_results):
     prices = list[str]()
+    if (response.status_code not in [200, 299]):
+        raise ValueError(f"Failed to get gas price from API. Returned HTTP Status Code: {response.status_code}")
+    
     data = response.json()
     if data and "resultado" in data and len(data["resultado"]) > 0:
         for result in data["resultado"]:
