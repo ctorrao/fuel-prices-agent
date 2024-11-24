47f6cb850657be9f216e07526f043083ea65fe46
Carlos Torrao <carlos.torrao@gmail.com>
2024-11-24 13:57:11 +0000

    Fix API response handling to ensure 'resultado' is not None before processing

--- backend/utils.py
+++ backend/utils.py
@@ -4,7 +4,7 @@ def process_api_fuel_price_response(response, max_fuel_results):
         raise ValueError(f"Failed to get gas price from API. Returned HTTP Status Code: {response.status_code}")
     
     data = response.json()
-    if data and "resultado" in data and len(data["resultado"]) > 0:
+    if data and "resultado" in data and data["resultado"] is not None and len(data["resultado"]) > 0:
         for result in data["resultado"]:
             # example: https://www.google.com/maps/search/39.223868,-9.076041
             # add a google maps link using latitude and longitude
