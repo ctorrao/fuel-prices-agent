c8772d8c3bf0aa5dda473801186f26f1bedd5b6c
Carlos Torrao <carlos.torrao@gmail.com>
2024-12-15 11:45:43 +0000

    tools import instead of from

--- backend/agent.py
+++ backend/agent.py
@@ -5,9 +5,9 @@ from langgraph.graph import START, StateGraph, MessagesState
 from langgraph.prebuilt import tools_condition, ToolNode
 
 #import inspect
-from tools import *
+import tools
 
-tools_for_llm = [get_districts, get_municipalities, get_brands, get_fuel_prices, get_fuel_prices_by_brand, convert_currency, available_currencies_to_convert]
+tools_for_llm = [tools.get_districts, tools.get_municipalities, tools.get_brands, tools.get_fuel_prices, tools.get_fuel_prices_by_brand, tools.convert_currency, tools.available_currencies_to_convert]
 #tools_for_llm = [func for name, func in inspect.getmembers(tools, inspect.isfunction)]
 
 # Define LLM with bound tools
